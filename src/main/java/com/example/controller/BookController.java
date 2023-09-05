5a67d5bd532efb3ece13b0b2aec078dc1d86ea87
silchenko-arsen <silchenkoarsen@gmail.com>
2023-09-05 17:38:18 +0300

    delete interface BookController

--- src/main/java/com/example/controller/BookController.java
+++ src/main/java/com/example/controller/BookController.java
@@ -2,13 +2,34 @@ package com.example.controller;
 
 import com.example.dto.BookDto;
 import com.example.dto.CreateBookRequestDto;
+import com.example.service.BookService;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-public interface BookController {
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/books")
+public class BookController {
+    private final BookService bookService;
 
-    List<BookDto> getAll();
+    @GetMapping
+    public List<BookDto> getAll() {
+        return bookService.findAll();
+    }
 
-    BookDto getBookById(Long id);
+    @GetMapping("/{id}")
+    public BookDto getBookById(@PathVariable Long id) {
+        return bookService.findById(id);
+    }
 
-    BookDto createBook(CreateBookRequestDto bookDto);
+    @PostMapping
+    public BookDto createBook(@RequestBody CreateBookRequestDto bookDto) {
+        return bookService.save(bookDto);
+    }
 }
