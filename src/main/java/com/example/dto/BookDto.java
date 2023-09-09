79ae05c341c64869b4f62cb34668ed00c42144b8
silchenko-arsen <silchenkoarsen@gmail.com>
2023-09-09 19:08:31 +0300

    do GlobalExceptionHandler and data validation

--- src/main/java/com/example/dto/BookDto.java
+++ src/main/java/com/example/dto/BookDto.java
@@ -1,14 +1,22 @@
 package com.example.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class BookDto {
     private Long id;
+    @NotBlank(message = "Title mustn't be null or empty")
     private String title;
+    @NotBlank(message = "Author mustn't be null or empty")
     private String author;
+    @NotBlank(message = "Isbn mustn't be null or empty")
     private String isbn;
+    @NotNull(message = "Price mustn't be null")
+    @Min(0)
     private BigDecimal price;
     private String description;
     private String coverImage;
